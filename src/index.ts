e8a2238d67d96dc9eebed07e1d67dcbc3d793d20
Federico Moretti <hello@federicomoretti.it>
2024-01-30 18:13:23 +0100

    feat: handle console closing

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,60 @@
-import chalk from 'chalk';
 import OpenAI from 'openai';
 import * as readline from 'readline';
+import chalk from 'chalk';
 
 /**
- * 
+ * Set OpenAI API key.
  */
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 /**
  * 
  */
-const main = async () => {
-  // 
+const main = () => {
+  const messages: any[] = [];
+
+  console.log(chalk.bold('Enter a user message below.'));
+
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
 
-  //
-  try {
-    rl.question('Prova?', async input => {
-      const chatCompletion = await openai.chat.completions.create({
+  rl.setPrompt('\nYou: ');
+  rl.prompt();
+
+  rl.on('line', async (input) => {
+    try {
+      const question = {
+        role: 'user',
+        content: input
+      }
+      messages.push(question);
+      
+      const completion = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
-        messages: [{ 'role': 'user', 'content': input }]
+        messages: messages
       });
+      
+      const answer = completion.choices[0].message.content;
+      
+      if (!!answer) {
+        console.log(chalk(`Bot: ${answer}`));
+        messages.push({
+          role: 'assistant',
+          content: answer
+        });
+      }
 
-      console.log(chatCompletion);
+      rl.prompt();
+    } catch (err) {
+      console.error(chalk.bold.red(`Error: ${err}`));
+    }
+  });
 
-    });
-  } catch (err) {
-    console.error(err);
-  }
+  rl.on('close', () => {
+    console.log('\nThank you!');
+  });
 };
 
 main();
