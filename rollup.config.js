053ec6df1c507fee30babe0691834f72dd72d8f0
Federico Moretti <hello@federicomoretti.it>
2024-01-31 19:07:30 +0100

    fix: update Rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from '@rollup/plugin-typescript';
 export default {
   input: ['./src/index.ts', './src/providers/openai.ts'],
   output: {
-    dir: 'dist',
+    dir: './dist',
     format: 'esm'
   },
   plugins: [
