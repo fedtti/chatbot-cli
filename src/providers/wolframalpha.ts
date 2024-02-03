3f51ec5c25f567d97fee8f5e966764606e519f7e
Federico Moretti <hello@federicomoretti.it>
2024-02-03 12:25:17 +0100

    fix: update Wolfram|Alpha provider

--- src/providers/wolframalpha.ts
+++ src/providers/wolframalpha.ts
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv';
-import * as needle from 'needle';
+import needle from 'needle';
 import { input } from '@inquirer/prompts';
 import chalk from 'chalk';
 
@@ -12,6 +12,31 @@ console.clear();
 const main = async () => {
   console.log(chalk.bold(`Enter a user message below.\n`));
 
+  try {
+    const question = await input({
+      message: chalk.blue('You: ')
+    });
+
+    question.toLowerCase() === 'exit' ? (console.log(chalk.bold(`\n\rHave a nice day.`)), process.exit(0)): 0;
+
+    encodeURIComponent(question).replace(/%20/g, '+');
+
+    const options = {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    }
+
+    const answer = await needle.get(`https://www.wolframalpha.com/api/v1/llm-api?input=${question}`, options);
+
+    if (!!answer) {
+      console.log(chalk.magenta('Bot: ') + answer);
+    }
+
+    main();
+  } catch (err) {
+    console.error(chalk.red.bold(`\n\r${err}.`));
+  }
 };
 
 main();
