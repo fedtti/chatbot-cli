9f34168577862840f278170e3be00428aa9e2db6
Federico Moretti <hello@federicomoretti.it>
2024-09-10 14:19:58 +0200

    fix: update JSDoc documentation

--- src/lib/utils/file-manager.ts
+++ src/lib/utils/file-manager.ts
@@ -2,10 +2,12 @@ import { promises as fs } from 'fs';
 import chalk from 'chalk';
 
 /**
- * Save the chat history into a `./chat-history.txt` file.
+ * Save the chat history into a './chat-history.txt' file.
  * @param {string} content - Chat history.
  */
 export const output = async (content: any): Promise<any> => {
+  // TODO: @fedtti - Format the chat history to be more human-readable.
+  
   try {
     await fs.writeFile('./chat-history.txt', content);
     console.info(chalk.bold(`\n\rThe chat history has been successfully saved in the './chat-history.txt' file.`));
