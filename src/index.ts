06e67edb61727f535fd67f49e61132c6f5921fc4
Federico Moretti <hello@federicomoretti.it>
2024-09-12 19:59:39 +0200

    feat: add initial support for Wolfram|Alpha LLM API

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@
 import * as dotenv from 'dotenv';
 import chalk from 'chalk';
 import figlet from 'figlet';
-import { main } from './providers/openai.js';
+import { main as chatGpt } from './providers/openai.js';
+import { main as wolframAlpha } from './providers/wolframalpha.js';
 
 dotenv.config(); // Ensure backward compatibility.
 
@@ -21,7 +22,7 @@ const init: any = (): void => {
 };
 
 /**
- * Ask the user to query the ChatGPT-4o model provided by Azure OpenAI Service¹.
+ * Ask the user to query the chosen model between ChatGPT-4o provided by Azure OpenAI Service and Wolfram|Alpha¹.
  */
 const run: any = (): void => {
   console.clear();
@@ -31,7 +32,7 @@ const run: any = (): void => {
     setTimeout(() => {
       console.clear();
       console.info(chalk.bold('Enter a user message below.\n\r'));
-      main();
+      chatGpt();
     }, 1000);
   } catch (error) {
     console.error(chalk.red.bold(`\n\r${error}`));
@@ -40,4 +41,4 @@ const run: any = (): void => {
 
 run();
 
-// ¹ More models will be available soon.
+// ¹More models will be available soon.
---
+++ src/providers/wolframalpha.ts
@@ -0,0 +1,8 @@
+import * as dotenv from 'dotenv';
+import { input, confirm, select } from '@inquirer/prompts';
+import chalk from 'chalk';
+import { list, prompt, output } from '../lib/utils/file-manager.js';
+
+export const main = async (): Promise<void> => {
+
+};
